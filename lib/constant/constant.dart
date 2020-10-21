d35c5cb39906dcda539d7461f2eeab47145ba9d6
I7Mustafa <mustafa7ibrahim@gmail.com>
2020-10-21 03:28:30 +0200

    remove an image

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -8,7 +8,6 @@ final CollectionReference userCollection = FirebaseFirestore.instance.collection
 List<String> imageDeals = <String>[
   'assets/images/deal1.png',
   'assets/images/deal2.png',
-  'assets/images/deal3.jpg',
   'assets/images/deal4.jpg',
   'assets/images/deal5.png'
 ];
